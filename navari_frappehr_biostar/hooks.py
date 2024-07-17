b97f8105e48e6054e7b85e9fa2d884f0c7b77dbf
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2024-07-17 07:09:01 +0300

    fix - Update hooks.py

--- navari_frappehr_biostar/hooks.py
+++ navari_frappehr_biostar/hooks.py
@@ -1,7 +1,7 @@
 app_name = "navari_frappehr_biostar"
 app_title = "Biostar Biometric Integration"
 app_publisher = "Navari Limited"
-app_description = "Suprema Biostar Biometric Integration with FrappeHR by Navari Ltd"
+app_description = "Suprema Biostar Biometric Integration with FrappeHR"
 app_email = "mwendwa@navari.co.ke"
 app_license = "GNU Affero General Public License v3.0"
 required_apps = ["frappe/erpnext"]
